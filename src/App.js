4f30b573758000f5932ae32f263ad472ef70697d
nintichance <mintidance@gmail.com>
2018-05-18 11:33:08 -0400

    Created Input.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Input from './components/Input'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello from App.js
+        <Input />
+      </div>
+    )
+  }
+}
+
+export default App
