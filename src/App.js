c199e162651fc97ca4b53728362e605adef00fdc
nintichance <mintidance@gmail.com>
2018-05-18 11:53:00 -0400

    Passed addTodo function to Input.js, which updates the state of App.js. Todos can be passed to List.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ class App extends Component {
   state = {
     todos: []
   }
+
+  addTodo = (todo) => {
+    console.log("addTodo Called")
+    this.setState((oldState)=>({todos: [...oldState.todos, todo] }))
+  }
+
   render() {
     return (
       <div>
         Hello from App.js
-        <Input />
-        <List />
+        <Input addTodo={this.addTodo}/>
+        <List todos={this.state.todos}/>
       </div>
     )
   }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
-
 class Input extends Component{
+    state = {
+        todo: ''
+    }
+
+    handleChange = (event) => {
+        event.preventDefault()
+        console.log(this.state.todo)
+        this.setState({ [event.target.name]: event.target.value })
+    }
+    addTodo = (event) => {
+        event.preventDefault()
+        this.props.addTodo(this.state.todo)
+    }
+
     render(){
         return(
             <div>
+                <form onSubmit={this.addTodo}>
                 <input 
                         name="todo"
                         placeholder="Enter Todo"
                         onChange={this.handleChange} />
+                <button className="input-button" type="submit" value="Login"> Submit </button>
+                </form>
                 Hello from Input
             </div>
         )
